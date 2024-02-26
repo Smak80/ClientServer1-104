205959973b0d0857c3935ce252bb5a0653650999
Sergey Makletsov <smaklets@kpfu.ru>
2024-02-26 08:43:44 +0300

    Версия от 19.02.2024

--- Server/src/Server.kt
+++ Server/src/Server.kt
@@ -1,2 +1,16 @@
-class Server {
+import java.net.ServerSocket
+import java.net.Socket
+
+class Server(var port: Int = 5104) {
+
+    private val serverSocket: ServerSocket
+
+    init{
+        serverSocket = ServerSocket(port)
+    }
+
+    fun start() {
+        val clientSocket: Socket = serverSocket.accept()
+        
+    }
 }
